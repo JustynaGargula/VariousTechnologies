327f71c3ef3be3e4051561f1233d1cdf5ae50e6d
JustynaGargula <s221391@student.uek.krakow.pl>
2025-03-26 10:08:32 +0100

    Started second task

--- Zadanie3/MessageRedirect/src/main/kotlin/Main.kt
+++ Zadanie3/MessageRedirect/src/main/kotlin/Main.kt
@@ -7,6 +7,8 @@ import io.ktor.client.statement.*
 suspend fun main() {
     val client = HttpClient(CIO)
     val response: HttpResponse = client.get("https://ktor.io/")
+//    val channel
+    val sendMessage: HttpResponse = client.post("https://discord.com/api/channels/{channel.id}/messages")
     println(response.status)
     client.close()
 }
