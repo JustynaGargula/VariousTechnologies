f194b4c0704cc7064bf14d32684603dd32874c96
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-24 17:55:55 +0200

    Added fetching products

--- Zadanie5/react-app/src/Components/Payment.js
+++ Zadanie5/react-app/src/Components/Payment.js
@@ -1,7 +1,12 @@
+import {useState} from "react";
+
 function Payment() {
+    const url = "http://localhost:1323/products";
     return (
         <div>
             <h1>Payment page</h1>
         </div>
     )
 }
+
+export default Payment
