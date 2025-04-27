c576454d3ba4dbe27a348fad36df49e2a82f8502
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-27 22:31:34 +0200

    Done Payment component

--- Zadanie5/react-app/src/index.js
+++ Zadanie5/react-app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Products from './Components/Products'
+import Payment from "./Components/Payment";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
     <Products />
+    <Payment></Payment>
   </React.StrictMode>
 );
 
