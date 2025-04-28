d1919877a02b474ddd59ead39cc00ebc854d1c67
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-28 11:28:02 +0200

    Cart component done

--- Zadanie5/react-app/src/Components/Cart.js
+++ Zadanie5/react-app/src/Components/Cart.js
@@ -1,7 +1,43 @@
+import {useState} from "react";
+import axios from "axios";
+
 function Cart() {
+    const url = "http://localhost:1323/cart";
+
+    const [cartItems, setCartItems] = useState([
+        { id: 1, name: 'Produkt A', quantity: 2, price: 50 },
+        { id: 2, name: 'Produkt B', quantity: 1, price: 100 },
+    ]);
+
+    const totalAmount = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+
+    const sendCart = async (err) => {
+        try {
+            const res = await axios.post(url, cartItems)
+            console.log("Successfully sent cart data: ", res.data);
+            alert("Successfully sent cart data");
+        }
+        catch(error) {
+            console.error("Sending cart data error:", error);
+            alert("Failed to send cart data");
+        }
+
+    };
+
     return (
         <div>
             <h1>Cart page</h1>
+            <h2>Items in cart:</h2>
+            <ul>
+                {cartItems.map(item => (
+                    <li id={item.id}>{item.name}, quantity: {item.quantity}, price: {item.price}</li>
+                ))}
+            </ul>
+            <p>Total: {totalAmount}</p>
+            <button onClick={sendCart}>Go to payment</button>
         </div>
     )
 }
+
+export default Cart;
--- Zadanie5/react-app/src/index.js
+++ Zadanie5/react-app/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Products from './Components/Products'
 import Payment from "./Components/Payment";
+import Cart from "./Components/Cart";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
+
+function Index() {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Products</Link> | <Link to="/cart">Cart</Link> | <Link to="/payment">Payment</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/payment" element={<Payment></Payment>}></Route>
+            </Routes>
+        </Router>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <Products />
-    <Payment></Payment>
+    <Index />
   </React.StrictMode>
 );
 
